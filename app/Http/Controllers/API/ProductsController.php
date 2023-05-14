58cc35eb390251b109ce814eebd5a3a5d33effef
abeer-samy8 <abeersami611@gmail.com>
2023-05-14 13:42:18 +0300

    Products Api

---
+++ app/Http/Controllers/API/ProductsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+use DB;
+use App\Http\Controllers\Controller;
+class ProductsController extends Controller
+{
+    function search(Request $request){
+        $q = $request->q;
+        $category=$request->category;
+        $query = product::whereRaw('active=1');
+        if($q){
+            $query->whereRaw('(title like ? or slug like ? or details like ? )',["%$q%","%$q%","%$q%"]);
+        }
+        if($category){
+            $query->whereIn('category_id',$category);
+        }
+        $products = $query->paginate(6)
+        ->appends([
+            'q'     =>$q,
+            'category' => $category
+        ]);
+
+        return $products;
+    }
+    
+    function details($id){
+        $item = Product::find($id);
+        if(!$item){
+            return response()->json(['status'=>0,'msg'=>'Invalid Product Id']);
+        }
+        return response()->json(['status'=>1,'item'=>$item]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ProductsController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/products",[ProductsController::class,'search']);
+Route::get("/product-details/{id}",[ProductsController::class,'details']);
